89bd876a32e4a85e87443de0a17f419f6a4608f9
Oğuzhan Kalelioğlu <oguzhankalelioglu@protonmail.com>
2022-02-06 23:34:28 +0300

    added app keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ["of2kRfElIvSydvfUk5YMeA","s3YCuWYRjV0P++w+ppwbkA","aJtIwPkhezWgqJa/Yr8JfQ", "om/KGav7ZVMVUgl54Svsqw"])
   },
 });
